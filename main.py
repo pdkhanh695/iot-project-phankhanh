23a99621d6225beae03e75976d59374c75bba42b
Khanh Phan <45734348+pdkhanh695@users.noreply.github.com>
2022-05-11 22:57:39 +0700

    Add files via upload
    Init commit - update gateway configuration

---
+++ main.py
@@ -0,0 +1,64 @@
+import serial.tools.list_ports
+import random
+import time
+import  sys
+from  Adafruit_IO import  MQTTClient
+
+AIO_FEED_ID = ["bbc-led", "bbc-fan"]
+AIO_USERNAME = "phankhanh"
+AIO_KEY = "aio_Tfki14MFPrT2R0rx5Sm4mdBbH3Tj"
+
+def  connected(client):
+    print("Ket noi thanh cong...")
+    for feed in AIO_FEED_ID:
+        client.subscribe(feed)
+
+def  subscribe(client , userdata , mid , granted_qos):
+    print("Subcribe thanh cong...")
+
+def  disconnected(client):
+    print("Ngat ket noi...")
+    sys.exit (1)
+
+def  message(client , feed_id , payload):
+    print("Nhan du lieu: " + payload)
+    ser.write((str(payload) + "#").encode())
+
+client = MQTTClient(AIO_USERNAME , AIO_KEY)
+client.on_connect = connected
+client.on_disconnect = disconnected
+client.on_message = message
+client.on_subscribe = subscribe
+client.connect()
+client.loop_background()
+
+# ser = serial.Serial( port=getPort(), baudrate=115200)
+ser = serial.Serial('/dev/cu.usbmodem113202', baudrate=115200)
+
+mess = ""
+def processData(data):
+    data = data.replace("!", "")
+    data = data.replace("#", "")
+    splitData = data.split(":")
+    print(splitData)
+    if splitData[1] == "TEMP":
+        client.publish("bbc-temp", splitData[2])
+
+mess = ""
+def readSerial():
+    bytesToRead = ser.inWaiting()
+    if (bytesToRead > 0):
+        global mess
+        mess = mess + ser.read(bytesToRead).decode("UTF-8")
+        while ("#" in mess) and ("!" in mess):
+            start = mess.find("!")
+            end = mess.find("#")
+            processData(mess[start:end + 1])
+            if (end == len(mess)):
+                mess = ""
+            else:
+                mess = mess[end+1:]
+
+while True:
+    readSerial()
+    time.sleep(1)
